df3a297e0450f7435e86c528f179c50deffcaa84
Daniy23 <115649015+Daniy23@users.noreply.github.com>
2024-04-08 00:51:55 -0600

    Final Names

--- cafeteria.py
+++ cafeteria.py
@@ -41,34 +41,34 @@ else:
 
 def test_validar_entrada():
   # 1. El nombre del artículo es alfabético (válido)
-  assert validar_entrada("Camisa,36,38,40") == True
+  assert validar_entrada("Cafe,36,38,40") == True
 
   # 2. El nombre del artículo tiene menos de 2 caracteres de longitud (inválido)
   assert validar_entrada("C,36,38,40") == False
 
   # 3. El nombre del artículo tiene de 2 a 15 caracteres de longitud (válido)
-  assert validar_entrada("Suéter largo, 42") == True
+  assert validar_entrada("Taro, 42") == True
 
   # 4. El valor del tamaño está en el rango de 1 a 48 (válido)
-  assert validar_entrada("Camisa,36,38,40") == True
+  assert validar_entrada("Chocolate,36,38,40") == True
 
   # 5. El valor del tamaño es un número entero (válido)
-  assert validar_entrada("Zapatos,39") == True
+  assert validar_entrada("Te,39") == True
 
   # 6. Los valores del tamaño se ingresan en orden ascendente (válido)
-  assert validar_entrada("Camisa,32,34,36,38,40") == True
+  assert validar_entrada("Agua,32,34,36,38,40") == True
 
   # 7. Se ingresan de uno a cinco valores de tamaño (válido)
-  assert validar_entrada("Bufanda,1,2,3,4,5") == True
+  assert validar_entrada("Expreso,1,2,3,4,5") == True
 
   # 8. El nombre del artículo es el primero en la entrada (válido)
-  assert validar_entrada("Bufanda, 42") == True
+  assert validar_entrada("Frappuccino, 42") == True
 
   # 9. Una sola coma separa cada entrada en la lista (válido)
-  assert validar_entrada("Camisa, 36, 38, 40") == True
+  assert validar_entrada("CaféCortado, 36, 38, 40") == True
 
   # 10. La entrada contiene o no espacios en blanco (a especificar en las pruebas)
-  assert validar_entrada("Camisa,36,38,40") == True
+  assert validar_entrada("Caramel Machiato,36,38,40") == True
 
 
  
